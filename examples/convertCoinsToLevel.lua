2d19c3de67321e4e784193a0cf964103a3625c63
Synitx <54985049+Synitx@users.noreply.github.com>
2023-06-21 02:14:14 -0500

    Delete convertCoinsToLevel.lua

--- examples/convertCoinsToLevel.lua
+++
@@ -1,7 +0,0 @@
-local floor,sqrt = math.floor, math.sqrt
-script.Parent.ProximityPrompt.Triggered:Connect(function(plr)
-    local val = plr.leaderstats["Coins"].Value
-    local level = plr.leaderstats["Level"].Value
-    plr.leaderstats["Level"].Value = level + floor(sqrt(val/100))
-    plr.leaderstats["Coins"].Value = 0
-end)
